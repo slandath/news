77248b18e790d276c0ce8935b9498a023c0e8964
slandath <jesus_slanda@yahoo.com>
2026-01-23 02:25:01 +0000

    feed working
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import * as nunjucks from 'nunjucks'
+import Parser from 'rss-parser'
 
 const app = new Hono()
+const parser = new Parser()
 
-app.get('/', (c) => {
-  return c.text('Hello World!')
+nunjucks.configure('src/templates', { autoescape: true })
+
+app.get('/', async (c) => {
+  try {
+    const feed = await parser.parseURL(
+      'https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=100003114',
+    )
+    const html = nunjucks.render('feeds.html', { feed })
+    return c.html(html)
+  }
+  catch (error) {
+    console.error(error)
+    return c.text('Error fetching feed')
+  }
 })
 
 serve(app)
