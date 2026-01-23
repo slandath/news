1b3eee7e9e9dc41f34c835ab6eccc6256d4463d7
slandath <jesus_slanda@yahoo.com>
2026-01-23 02:49:57 +0000

    home page and feeds
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,28 @@ import { Hono } from 'hono'
 import * as nunjucks from 'nunjucks'
 import Parser from 'rss-parser'
 
-const app = new Hono()
-const parser = new Parser()
-
 nunjucks.configure('src/templates', { autoescape: true })
 
-app.get('/', async (c) => {
+const app = new Hono()
+const parser = new Parser()
+const feeds = [
+  { title: 'The Verge', url: 'https://www.theverge.com/rss/index.xml' },
+  { title: 'CNBC', url: 'https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=100003114' },
+  { title: 'Hacker News', url: 'https://news.ycombinator.com/rss' },
+].sort((a, b) => a.title.localeCompare(b.title))
+
+app.get('/', (c) => {
+  const html = nunjucks.render('home.html', { feeds })
+  return c.html(html)
+})
+
+app.get('/feed', async (c) => {
+  const feedURL = c.req.query('url') || feeds[0].url
   try {
     const feed = await parser.parseURL(
-      'https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=100003114',
+      feedURL,
     )
-    const html = nunjucks.render('feeds.html', { feed })
+    const html = nunjucks.render('feeds.html', { feed, feeds })
     return c.html(html)
   }
   catch (error) {
