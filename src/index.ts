05338beb1234e7d96d80847ddb34eb7a11c8475e
Tom Slanda <jesus_slanda@yahoo.com>
2026-01-23 16:14:58 -0500

    Chore/readme update (#9)
    * readme update
    
    Signed-off-by: slandath <jesus_slanda@yahoo.com>
    
    * added css
    
    Signed-off-by: slandath <jesus_slanda@yahoo.com>
    
    ---------
    
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { serve } from '@hono/node-server'
 import Handlebars from 'handlebars'
 import { Hono } from 'hono'
 import Parser from 'rss-parser'
+import { feeds } from './feeds.js'
 
 const app = new Hono()
 const parser = new Parser()
@@ -19,13 +20,6 @@ const homeTemplate = Handlebars.compile(
 const feedTemplate = Handlebars.compile(
   fs.readFileSync(join(templateDir, 'feeds.html'), 'utf-8'),
 )
-
-const feeds = [
-  { title: 'The Verge', url: 'https://www.theverge.com/rss/index.xml' },
-  { title: 'CNBC', url: 'https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=100003114' },
-  { title: 'Hacker News', url: 'https://news.ycombinator.com/rss' },
-  { title: 'NBC News', url: 'https://feeds.nbcnews.com/nbcnews/public/news' },
-].sort((a, b) => a.title.localeCompare(b.title))
 
 app.get('/', (c) => {
   const feedsWithEncoded = feeds.map(f => ({
